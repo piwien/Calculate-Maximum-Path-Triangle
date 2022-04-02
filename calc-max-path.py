2e5211c0f92aebdd20488b6d195f2b6b28e46a14
Berkay <71926128+piwien@users.noreply.github.com>
2022-04-02 21:17:43 +0300

    Update calc-max-path.py

--- calc-max-path.py
+++ calc-max-path.py
@@ -12,21 +12,20 @@ def isprime(number):
 
 def maxPathSumTriangle(arr):
     newlist = [row[:] for row in arr]
-    print(newlist)
     for row in range(len(arr)-2, -1, -1):
         for col in range(len(arr[row])):
             if (isprime(newlist[row][col]) or (isprime(newlist[row+1][col]) and isprime(newlist[row+1][col+1]))):
                 continue
-            elif (isprime(newlist[row+1][col]) == False and isprime(newlist[row+1][col+1]) == False):
+            elif (isprime(newlist[row+1][col]) == False and isprime(newlist[row+1][col+1]) == False): #Both numbers not prime 
                  arr[row][col] += max(arr[row+1][col], arr[row+1][col+1])
-            elif (isprime(newlist[row+1][col]) == False and isprime(newlist[row+1][col+1]) == True):
+            elif (isprime(newlist[row+1][col]) == False and isprime(newlist[row+1][col+1]) == True): #Number1 not prime Number2 prime 
                 arr[row][col] += arr[row+1][col]
-            elif (isprime(newlist[row+1][col]) == True and isprime(newlist[row+1][col+1]) == False):
+            elif (isprime(newlist[row+1][col]) == True and isprime(newlist[row+1][col+1]) == False): #Number2 not prime Number1 prime
                 arr[row][col] += arr[row+1][col+1]
-    return arr[0][0]
+    return f"Maximum Path is: {arr[0][0]}"
 
 with open("input.txt") as f:
     arr = []
     for line in f:
         arr.append(list(map(int, line.split(" "))))
-    print("Maximum Path is: " maxPathSumTriangle(arr))
+    print(maxPathSumTriangle(arr))
